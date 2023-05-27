94a1053a1d0997f7bb072852c0a114ae42cbdd7b
Ajinkya Khadilkar <ajinkyakhadilkar1997@gmail.com>
2023-05-28 00:48:04 +0530

    remove unused function

--- InPainting/scripts/inpainting.py
+++ InPainting/scripts/inpainting.py
@@ -40,15 +40,6 @@ def parse_args():
     return opt
 
 
-def put_watermark(img, wm_encoder=None):
-    if wm_encoder is not None:
-        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-        img = wm_encoder.encode(img, 'dwtDct')
-        img = Image.fromarray(img[:, :, ::-1])
-    return img
-
-
-
 def initialize_model(config, ckpt):
     config = OmegaConf.load(config)
     model = instantiate_from_config(config.model)
